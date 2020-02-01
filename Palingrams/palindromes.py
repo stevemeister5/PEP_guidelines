8118d9c38e4887965f2a0a629e3c933ead878115
stevemeister5 <stephenreubenm@gmail.com>
2020-02-01 11:15:15 +0300

    Palingram setup

---
+++ Palingrams/palindromes.py
@@ -0,0 +1,11 @@
+"""Find palindromes(letter palingrams) in a dictionary file."""
+import load_dictionary
+word_list = load_dictionary.load('Palingrams/american-english.txt')
+pali_list = []
+
+for word in word_list:
+    if len(word) > 1 and word == word[::-1]:
+        pali_list.append(word)
+
+print("\nNumber of palindroms found = {}\n".format(len(pali_list)))
+print(*pali_list, sep='\n')
