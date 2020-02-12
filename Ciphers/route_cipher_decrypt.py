0539674e7f65b702497786ba95ff98ff3b584ea3
stevemeister5 <stephenreubenm@gmail.com>
2020-02-12 16:54:48 +0300

    Complete decrypt.py

--- Ciphers/route_cipher_decrypt.py
+++ Ciphers/route_cipher_decrypt.py
@@ -63,3 +63,59 @@ def main():
 
     print("Plaintext = {}".format(plaintext))
 
+def validate_col_row(cipherlist):
+    """Check that input columns & rows are valid vs message length"""
+    factors = []
+    len_cipher = len(cipherlist)
+    for i in range(2, len_cipher): # range excludes 1 column ciphers
+        if len_cipher % i == 0:
+            factors.append(i)
+    print("\nLength of cipher = {}".format(len_cipher))
+    print("Acceptable column/row values include: {}".format(factors))
+    print()
+    if ROWS * COLS != len_cipher:
+        print("\nError - Input columns & rows not factors of length"
+            "of cipher. Terminating program.", file=sys.stderr)
+        sys.exit(1)
+
+
+def key_to_int(key):
+    """Turn key into list of integers & check validity"""
+    key_int = [int(i) for i in key.split()]
+    key_int_lo = min(key_int)
+    key_int_hi = max(key_int)
+    if len(key_int) != COLS or key_int_lo < -COLS or key_int_hi > COLS \
+        or 0 in key_int:
+        print("\nError - Problem with key. Terminating.", file=sys.stderr)
+        sys.exit(1)
+    else:
+        return key_int 
+
+def build_matrix(key_int, cipherlist):
+    """Turn every n items in a list into a new item in a list of lists."""
+    translation_matrix = [None] * COLS
+    start = 0
+    stop = ROWS
+    for k in key_int:
+        if k < 0: # read bottom-to-top of column
+            col_items = cipherlist[start: stop]
+        elif k > 0: # reaf top-to-bottom of column
+            col_items = list((reversed(cipherlist[start:stop])))
+        translation_matrix[abs(k) - 1 ] = col_items
+        start += ROWS
+        stop += ROWS 
+    return translation_matrix
+
+def decrypt(translation_matrix):
+    """Loop through nested lists popping off last item to a string"""
+    plaintext=''
+    for i in range(ROWS):
+        for matrix_col in translation_matrix:
+            word = str(matrix_col.pop())
+            plaintext += word + ''
+    return plaintext
+
+
+if __name__ == '__main__':
+    main() 
+
